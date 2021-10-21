2e6cdff7b3887a8f33dd10b8707db3b210b983e5
ChristophWinklerHub <e11701292@student.tuwien.ac.at>
2021-10-21 20:57:48 +0200

    added Vosk dependencies

---
+++ app/src/main/java/at/ac/tuwien/wave/Vosk.java
@@ -0,0 +1,35 @@
+package at.ac.tuwien.wave;
+
+import org.vosk.android.RecognitionListener;
+
+public class Vosk implements RecognitionListener {
+
+    public Vosk() {
+
+    }
+
+    @Override
+    public void onPartialResult(String s) {
+
+    }
+
+    @Override
+    public void onResult(String s) {
+
+    }
+
+    @Override
+    public void onFinalResult(String s) {
+
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    @Override
+    public void onTimeout() {
+
+    }
+}
